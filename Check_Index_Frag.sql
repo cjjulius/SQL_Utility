a9e61034487d313915b8ed31ccf5fa1a21d4672e
cjjulius <cjdoootjuliusatttoutlookddoottcom>
2025-07-09 17:45:33 -0700

    Added Check_Index_Frag.sql to readme. Added Check_Index_Frag.sql and sanatized.

---
+++ Check_Index_Frag.sql
@@ -0,0 +1,48 @@
+---------------------
+-- Charlton E Julius
+-- Created: 2016-03-30
+-- Purpose: Index Fragmentation
+---------------------
+
+/*
+
+----------ALL INDEXES---------
+
+*/
+
+SELECT  OBJECT_NAME(ind.object_id) AS TableName
+       ,ind.name AS IndexName
+       ,indexstats.index_type_desc AS IndexType
+       ,indexstats.avg_fragmentation_in_percent
+	   ,indexstats.page_count
+FROM    sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, NULL) indexstats
+INNER JOIN sys.indexes ind ON ind.object_id = indexstats.object_id
+                              AND ind.index_id = indexstats.index_id
+WHERE   indexstats.avg_fragmentation_in_percent > 50
+ORDER BY indexstats.avg_fragmentation_in_percent DESC, indexstats.page_count DESC;
+
+
+
+
+/*
+--------SINGLE OBJECT---------
+*/
+
+SELECT [database_id]
+        ,ind.[object_id]
+        ,ind.[index_id]
+        ,[partition_number]
+        ,[index_type_desc]
+        ,[index_depth]
+        ,[index_level]
+        ,[avg_fragmentation_in_percent]
+        ,[fragment_count]
+        ,[avg_fragment_size_in_pages]
+        ,[page_count]
+        ,ind.name
+ FROM sys.dm_db_index_physical_stats(DB_ID(N'SomeDB'), OBJECT_ID(N'dbo.SomeTable'), NULL, NULL , 'LIMITED')
+	INNER JOIN sys.indexes ind ON ind.object_id = dm_db_index_physical_stats.object_id
+                                AND ind.index_id = dm_db_index_physical_stats.index_id;
+--WHERE   [avg_fragmentation_in_percent] > 10.0
+--		AND [index_id] > 0
+--		AND [page_count] > 100
--- README.md
+++ README.md
@@ -19,9 +19,12 @@ Shows all temp tables in SQL Server along with information about them.
 
 ##  Latches.sql
 Shows all the latches that are waiting. Set the percentage threshold at the bottom based on preferences. 95% I feel is a pretty good number, though.
-
+Check_Index_Frag.sql
 ## States_Territories_US.sql
 Creates a table and inserts US states and territories into it. I couldn't find anything that did exactly this with what I needed, so here it is.
 
 ## Tables_Heaps.sql
 Shows all tables that are heaps in the current database.
+
+## Check_Index_Frag.sql
+Shows fragmentation of all indexes. used 50% as a fragmentation limit to start. Second script to target a single table object.
